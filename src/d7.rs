493f42a1f5222fc58a2aeaa519b28249a4d0870b
Greg Ellson <gellson@inkbit3d.com>
2023-04-21 08:14:58 -0400

    start cd parsing

--- src/d7.rs
+++ src/d7.rs
@@ -1,23 +1,66 @@
 use crate::utils::read_lines;
+use std::fs::File;
+use std::io::BufReader;
+use std::{collections::HashMap, io::Lines};
 
-fn p1() {
-    // read file
-    if let Ok(lines) = read_lines("inputs/7.txt") {
-        
+struct Obj {
+    name: String,
+    size: i32,
+}
+enum DiskObject {
+    Dir(HashMap<String, DiskObject>),
+    Obj(Obj),
+}
+
+// fn split_line()
+
+fn p1(lines: Lines<BufReader<File>>) {
+    let mut disk: HashMap<String, DiskObject> = HashMap::new();
+    let mut cur: Vec<&str> = Vec::new();
+    let mut lines = lines.into_iter();
+    if let Some(Ok(line)) = lines.next() {
+        let mut words = line.split_whitespace();
+        let first_word = words.next().unwrap();
+        match first_word {
+            "$" => {
+                let second_word = words.next().unwrap();
+                match second_word {
+                    "cd" => {
+                        let third_word = words.next().unwrap();
+                        match third_word {
+                            "/" => {
+                                cur = Vec::new();
+                                
+                            },
+                            ".." => {
+                                cur.pop();
+                            }
+                            _ => {
+                                cur.push(third_word);
+                            }
+                        }
+                    },
+                    "ls" => {},
+                    _ => {}
+                }
+            },
+            "dir" => {}
+            _ => {}
+        }
     }
 }
 
-fn p2() {
-    // read file
-    if let Ok(lines) = read_lines("inputs/7.txt") {
-
-    }
+fn p2(lines: Lines<BufReader<File>>) {
+    
 }
 
 pub fn d7(part: i32) {
-    match part {
-        1 => p1(),
-        2 => p2(),
-        _ => println!("Invalid part"),
+    // read file
+    if let Ok(lines) = read_lines("inputs/7.txt") {
+        match part {
+            1 => p1(lines),
+            2 => p2(lines),
+            _ => println!("Invalid part"),
+        }
     }
 }

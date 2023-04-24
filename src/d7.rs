0ca955ff863b609aea2f6ab486614c9795668634
Greg Ellson <gellson@inkbit3d.com>
2023-04-23 20:45:17 -0400

    d7 p2 done with help

--- src/d7.rs
+++ src/d7.rs
@@ -3,16 +3,16 @@ use std::iter::Peekable;
 fn sum_node(lines: &mut Peekable<impl Iterator<Item = &'static str>>, sum: &mut i32) -> i32 {
     let mut size = 0;
     while let Some(line) = lines.next() {
-            match line {
-                "$ cd .." => break,
-                _ if line.starts_with("$ ls") => {
-                    size = std::iter::from_fn(|| lines.next_if(|line| !line.starts_with('$')))
+        match line {
+            "$ cd .." => break,
+            _ if line.starts_with("$ ls") => {
+                size = std::iter::from_fn(|| lines.next_if(|line| !line.starts_with('$')))
                     .filter(|line| !line.starts_with('d'))
                     .map(|line| line.split(' ').next().unwrap().parse::<i32>().unwrap())
                     .sum()
-                }
-                _ => size += sum_node(lines, sum)
             }
+            _ => size += sum_node(lines, sum),
+        }
     }
     if size <= 100000 {
         *sum += size;
@@ -20,21 +20,62 @@ fn sum_node(lines: &mut Peekable<impl Iterator<Item = &'static str>>, sum: &mut 
     return size;
 }
 
-fn p1(mut lines: Peekable<impl Iterator<Item = &'static str>>) {
+fn p1() {
+    let mut lines = include_str!("../inputs/7.txt").lines().peekable();
     let mut sum = 0;
     sum_node(&mut lines, &mut sum);
     println!("{}", sum)
 }
 
-pub fn d7(part: i32) {
-    // read file    
-    // if let Ok(lines) = read_lines("inputs/7.txt") {
-    //     let mut lines = lines.into_iter().peekable();
-        let lines = include_str!("../inputs/7.txt").lines().peekable();
-        match part {
-            1 => p1(lines),
-            // 2 => p2(lines),
-            _ => println!("Invalid part"),
+const FS_TOTAL: u64 = 70000000;
+const UP: u64 = 30000000;
+
+#[derive(Default)]
+struct Dir {
+    dirs: Vec<Dir>,
+    size: u64,
+}
+
+fn build_dirs(lines: &mut Peekable<impl Iterator<Item = &'static str>>) -> Dir {
+    let mut dirs: Dir = Dir::default();
+
+    while let Some(line) = lines.next() {
+        match line {
+            "$ cd .." => break,
+            _ if line.starts_with("$ ls") => {
+                dirs.size = std::iter::from_fn(|| lines.next_if(|line| !line.starts_with('$')))
+                    .filter(|line| !line.starts_with('d'))
+                    .map(|line| line.split(' ').next().unwrap().parse::<u64>().unwrap())
+                    .sum()
+            }
+            _ => dirs.dirs.push(build_dirs(lines))
         }
+    }
+    dirs.size += dirs.dirs.iter().map(|d| d.size).sum::<u64>();
+    return dirs;
+}
+
+fn search(dirs: &Dir, min: u64) -> Option<u64> {
+    dirs.dirs.iter()
+        .filter(|d| d.size >= min)
+        .flat_map(|d| [Some(d.size), search(d, min)])
+        .flatten()
+        .min()
+}
+
+fn p2() {
+    let mut lines = include_str!("../inputs/7.txt").lines().peekable();
+    let dirs = build_dirs(&mut lines);
+    let min = UP - (FS_TOTAL-dirs.size);
+    let min_dir = search(&dirs, min).unwrap();
+    println!("{}", min_dir);
+}
+
+pub fn d7(part: i32) {
+    match part {
+        1 => p1(),
+        2 => p2(),
+        _ => println!("Invalid part"),
+    }
     // }
 }
